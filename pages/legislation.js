7cd4a29f86045a50ebcd310a5dd7620d558b0d35
MattAxon <mattaxon2@gmail.com>
2022-03-15 16:58:02 -0400

    Initial out subtree commit gh-pages

---
+++ pages/legislation.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+export default function legislation() {
+  return (
+    <div>
+      <Head>
+        <title>legislation</title>
+      </Head>
+      <body>
+        <article className="text-center">Legislation</article>
+      </body>
+    </div>
+  );
+}
---
+++ pages/policy.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+export default function policy() {
+  return (
+    <div>
+      <Head>
+        <title>legislation</title>
+      </Head>
+      <body>
+        <article className='text-center'>
+          policy
+        </article>
+      </body>
+    </div>
+  );
+}
