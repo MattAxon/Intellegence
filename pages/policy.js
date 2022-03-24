48718ecdd11e8a4d1abe6227256eff104795821c
MattAxon <mattaxon2@gmail.com>
2022-03-23 23:26:16 -0400

    responsive changes

--- pages/policy.js
+++ pages/policy.js
@@ -9,7 +9,7 @@ export default function policy() {
       <div>
         <h1 className="text-center text-lg my-8">Policy</h1>
         <div>
-          <p className="text-center mx-20">
+          <p className="text-center mx-8 lg:mx-20">
             Since the 1960's the United State's intelligence community has
             almost certainly progressed. Prior to Nixon, the intelligence
             community was less of a community and more of a collection of
@@ -23,15 +23,12 @@ export default function policy() {
             within the intelligence community has been a key focus of policy
             makers and the focus of the US intelligence community has been on
             gathering information for their respective military branch or
-            department. The SCOTUS holds protecting American foreign policy
-            interests and national security in very high reguard and as a result
-            have ruled in favor of the intelligence community in almost every
-            lawsuit brought against them whether it be{" "}
-            <i>Nixon v. Administrator of General Services</i> or{" "}
-            <i>Federal Bureau of Investigation v. Fazaga.</i> Most recently the
-            intelligence community has directed its attention towards the
-            ongoing war in Ukraine, becoming a massive headache for Putin by
-            calling his moves before he makes them and sharing their
+            department.
+          </p>
+          <p className="text-center mt-4 mx-8 lg:mx-20">
+            Most recently, the intelligence community has directed its attention
+            towards the ongoing war in Ukraine, becoming a massive headache for
+            Putin by calling his moves before he makes them and sharing their
             intelligence with the Ukrainian armed forces. This is however coming
             a few months after one of the greatest failures of the past decade
             in Afganistan so despite a good showing so far in eastern Europe the
