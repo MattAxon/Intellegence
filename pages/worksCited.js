be892fa6e856df3950229a78e5cd04365f9708f3
MattAxon <mattaxon2@gmail.com>
2022-03-27 11:13:08 -0400

    fixed margin of works cited 3

--- pages/worksCited.js
+++ pages/worksCited.js
@@ -7,39 +7,39 @@ export default function WorksCited() {
       </Head>
       <div className="mb-4">
         <h1 className="text-center text-lg my-8">Works Cited</h1>
-        <div>
-          <p className="text-center mb-2">
+        <div className="w-full">
+          <p className="text-center mb-2 mx-2">
             <i>
               “The Evolution of the U.S. Intelligence Community-An Historical
               Overview.” The Evolution of the U.S. Intelligence Community-an
               Historical Overview, irp.fas.org/offdocs/int022.html.
             </i>
           </p>
-          <p className="text-center mb-2">
+          <p className="text-center mb-2 mx-2">
             <i>
               H.R.1555 - 106th Congress (1999-2000): Intelligence.
               www.congress.gov/bill/106th-congress/house-bill/1555.
             </i>
           </p>
-          <p className="text-center mb-2">
+          <p className="text-center mb-2 mx-2">
             <i>
               “Major Intelligence Related Statutes.” Major Intelligence Related
               Statutes | Intelligence Committee,
               www.intelligence.senate.gov/laws/related-statutes.
             </i>
           </p>
-          <p className="text-center mb-2">
+          <p className="text-center mb-2 mx-2">
             <i>
               Office of the Clerk, U.S. House of
               Representatives.clerk.house.gov/evs/1999/roll555.xml.
             </i>
           </p>
-          <p className="text-center mb-2">
+          <p className="text-center mb-2 mx-2">
             <i>
               “Federal Bureau of Investigation v. Fazaga” Oyez, www.oyez.org/.
             </i>
           </p>
-          <p className="text-center mb-2">
+          <p className="text-center mb-2 mx-2">
             <i>
               "Central Intelligence Agency v. Sims." Oyez,
               www.oyez.org/cases/1984/83-1075. Accessed 23 Mar. 2022.
